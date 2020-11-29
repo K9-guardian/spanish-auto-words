34deaa39d883464af0d98b986c59a64c4a4694f9
Krishna Girkar <krishna.g10rules@gmail.com>
2020-11-28 17:20:09 -0800

    Fix upload spinner

--- app.js
+++ app.js
@@ -107,7 +107,6 @@ async function uploadQuizlet() {
     if (words == null || words.length == 0) {
         console.log('Bad URL')
         $('#upload').prop('disabled', false)
-        removeUploadSpinner()
         return
     }
 
