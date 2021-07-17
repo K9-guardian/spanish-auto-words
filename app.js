278e7b80943ef6b3514ad54a284ff65820b002c7
Krishna Girkar <krishna.g10rules@gmail.com>
2021-07-16 17:19:38 -0700

    Bruh

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ async function getTranslation(english) {
 
 async function getDictionary(word) {
     const dictionaryURL = "https://gdjosxqauk.execute-api.us-west-1.amazonaws.com/default/getDictionary?word="
-    word = word.replace(/\bto\b|\[.*\]|\(.*\)/, '').split(/(\bor\b|,)/)[0]
+    word = word.replace(/\bto\b|\[.*\]|\(.*\)/, '').split(/\bor\b|,/)[0]
     const response = await fetch(dictionaryURL + word, {
         headers: {
             "x-api-key": "AtTjaZXXnc33G3ZBX18d6BGEJAkgPhS88YgEAnOj"
